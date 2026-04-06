b4ad8bf378c301c8cc8f8bd148fffc890b56f9c4
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-06 11:24:31 +0700

    mendefinisikan struct alamatDetail dan mahasiswa

---
+++ struct4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string Desa;
+    string Kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    
+}
