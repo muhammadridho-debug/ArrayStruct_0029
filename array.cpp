9d3855da623197cbb719b55dcf5d080771806e6d
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-06 10:49:51 +0700

    menambahkan fitur input dan output manual untuk array datamu

--- array.cpp
+++ array.cpp
@@ -15,4 +15,24 @@ int main() {
     // mengisi array
     dataku[1] = 200;
     cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+
+    // mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+
+    //menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+
 }
