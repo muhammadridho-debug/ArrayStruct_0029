990c553231860d04b0745fbe2492015653cdb278
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-06 10:48:05 +0700

    menambahkan inisialisasi dan manipulasi array dasar pada dataku

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // deklarasi array disertai inisialisasi
+    int dataku[5] = {10, 20, 30, 40, 50};
+    
+    // deklarasi tanpa inisialisasi
+    int datamu[5];
+
+    // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+}
