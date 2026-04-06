7e99ca03c76ced1625a42a4f291ba37009948854
muhammadridho-debug <idomuhammad11@gmail.com>
2026-04-06 11:15:50 +0700

    buat struct alamatDetail dan mahasiswa

---
+++ struct3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+    
+}
